9e20751622afdc3815accce6e78cc5b3096aaf5a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/default.aspx
@@ -0,0 +1,69 @@
+<%@ Page Language="vb" AutoEventWireup="true" CodeFile="default.aspx.vb" Inherits="_default" %>
+
+<%@ Register Assembly="DevExpress.Web.v9.3" Namespace="DevExpress.Web.ASPxRoundPanel"
+	TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.v9.3" Namespace="DevExpress.Web.ASPxPanel"
+	TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxEditors.v9.3" Namespace="DevExpress.Web.ASPxEditors"
+	TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.v9.3" Namespace="DevExpress.Web.ASPxDataView"
+	TagPrefix="dx" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>How to add/remove/edit DataView items on a callback</title>
+	<style type="text/css">
+		.combo { 
+			display: inline-block;
+		}
+		.btn { 
+			border: 0;
+		}
+	</style>
+
+	<script type="text/javascript" language="javascript">
+		function DeleteItem(id) {
+			if(confirm('Do you want to delete this item?')) {
+				dataView.PerformCallback('delete=' + id);    
+			}
+		}
+		function AddItem(s, e) {
+			dataView.PerformCallback('add');
+		}
+	</script>
+
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			<dx:ASPxDataView ID="dataView" runat="server" Width="100%" AllowPaging="False" ColumnCount="1"
+				ItemSpacing="32px" ClientInstanceName="dataView" EnableViewState="false" OnCustomCallback="dataView_CustomCallback">
+				<ItemTemplate>
+					<dx:ASPxComboBox runat="server" ValueType="System.Int32" ID="comboItemValue" CssClass="combo"
+						Value='<%#Eval("ItemValue")%>'>
+						<Items>
+							<dx:ListEditItem Text="(none)" Value="-1" />
+							<dx:ListEditItem Text="One" Value="1" />
+							<dx:ListEditItem Text="Two" Value="2" />
+							<dx:ListEditItem Text="Three" Value="3" />
+						</Items>
+					</dx:ASPxComboBox>
+					<a runat="server" id="deleteLink" href='<%#GetDeleteLink(Eval("Id"))%>'>
+						<img src="delete.gif" alt="Delete Button" title="Delete" width="16" height="16" class="btn" /></a>
+				</ItemTemplate>
+				<ItemStyle BackColor="Transparent" Height="0px" Width="200px">
+					<Paddings Padding="0px" />
+					<Border BorderStyle="None" />
+				</ItemStyle>
+				<Paddings Padding="20px" />
+			</dx:ASPxDataView>
+			<dx:ASPxButton ID="ASPxButton1" runat="server" AutoPostBack="False" Text="Add Item">
+				<ClientSideEvents Click="AddItem" />
+			</dx:ASPxButton>
+			<br />
+			<dx:ASPxButton ID="btSave" runat="server" OnClick="btSave_Click" Text="Save">
+			</dx:ASPxButton>
+		</div>
+	</form>
+</body>
+</html>
---
+++ VB/WebSite/default.aspx.vb
@@ -0,0 +1,125 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxDataView
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class _default
+	Inherits System.Web.UI.Page
+	' Data 
+	Public Class MyItem
+		Private id_Renamed As String
+		Private itemValue_Renamed As Integer
+
+		Public Property Id() As String
+			Get
+				Return id_Renamed
+			End Get
+			Set(ByVal value As String)
+				id_Renamed = value
+			End Set
+		End Property
+		Public Property ItemValue() As Integer
+			Get
+				Return itemValue_Renamed
+			End Get
+			Set(ByVal value As Integer)
+				itemValue_Renamed = value
+			End Set
+		End Property
+		Public Sub New()
+			Me.New(-1)
+		End Sub
+		Public Sub New(ByVal itemValue As Integer)
+			Me.id_Renamed = Guid.NewGuid().ToString()
+			Me.itemValue_Renamed = itemValue
+		End Sub
+	End Class
+	Public Class MyItems
+		Inherits List(Of MyItem)
+
+		Public Sub New()
+			MyBase.New()
+			' Adding sample data
+			Add(New MyItem(1))
+			Add(New MyItem(2))
+			Add(New MyItem(3))
+		End Sub
+		Public Function NewItem() As MyItem
+			Dim item As New MyItem()
+			Add(item)
+			Return item
+		End Function
+		Public Sub DeleteById(ByVal id As String)
+			For i As Integer = 0 To Count - 1
+				Dim item As MyItem = Me(i)
+				If item.Id = id Then
+					RemoveAt(i)
+					Exit For
+				End If
+			Next i
+		End Sub
+	End Class
+
+	Public ReadOnly Property Data() As MyItems
+		Get
+			If Session("MyItems") Is Nothing Then
+				Session("MyItems") = New MyItems()
+			End If
+			Return CType(Session("MyItems"), MyItems)
+		End Get
+	End Property
+	' End Data 
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		dataView.DataSource = Data
+		dataView.DataBind()
+	End Sub
+
+	Protected Sub dataView_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+		UpdateMyItemsFromClient()
+
+		If e.Parameter.StartsWith("delete") Then
+			Dim parts() As String = e.Parameter.Split(New String() { "=" }, StringSplitOptions.RemoveEmptyEntries)
+			Dim id As String = parts(1)
+			Data.DeleteById(id)
+		Else
+			If e.Parameter = "add" Then
+				Data.NewItem()
+			End If
+		End If
+
+		dataView.DataBind()
+	End Sub
+	Protected Sub btSave_Click(ByVal sender As Object, ByVal e As EventArgs)
+		UpdateMyItemsFromClient()
+		' Here you can save data to a database
+	End Sub
+
+	Protected Function GetDeleteLink(ByVal id As Object) As String
+		Dim s As String
+		If (id IsNot Nothing) Then
+			s = id.ToString()
+		Else
+			s = ""
+		End If
+		Return String.Format("javascript:DeleteItem('{0}');", s)
+	End Function
+	Protected Sub UpdateMyItemsFromClient()
+		For i As Integer = 0 To dataView.VisibleItems.Count - 1
+			Dim item As DataViewItem = dataView.Items(i)
+			Dim comboBox As ASPxComboBox = CType(dataView.FindItemControl("comboItemValue", item), ASPxComboBox)
+
+			Data(i).ItemValue = CInt(Fix(comboBox.Value))
+		Next i
+	End Sub
+End Class
